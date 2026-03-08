621035acfe6baed68b3413a6ee3aa09112ccc655
Jayaram-A-H <me21b079@smail.iitm.ac.in>
2026-03-08 23:18:59 +0530

    2 cameras streaming thru tcp/ip

--- Simulator/CameraFeed.py
+++ Simulator/CameraFeed.py
@@ -2,54 +2,55 @@ import socket
 import struct
 import cv2
 import numpy as np
+import threading
 
-HOST = "0.0.0.0"
-PORT = 5000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(1)
+def receive_camera(port, window_name):
 
-print("Waiting for Unity connection...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("0.0.0.0", port))
+    server.listen(1)
 
-conn, addr = server.accept()
-print("Connected:", addr)
+    print("Waiting for camera on port", port)
 
-data = b""
+    conn, addr = server.accept()
+    print("Connected:", addr)
+
+    data = b""
+
+    while True:
+
+        while len(data) < 4:
+            packet = conn.recv(4096)
+            if not packet:
+                return
+            data += packet
+
+        packed_size = data[:4]
+        data = data[4:]
+        frame_size = struct.unpack("I", packed_size)[0]
+
+        while len(data) < frame_size:
+            data += conn.recv(4096)
+
+        frame_data = data[:frame_size]
+        data = data[frame_size:]
+
+        frame = cv2.imdecode(
+            np.frombuffer(frame_data, dtype=np.uint8),
+            cv2.IMREAD_COLOR
+        )
+
+        if frame is not None:
+            cv2.imshow(window_name, frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+
+threading.Thread(target=receive_camera, args=(4000,"Cam1")).start()
+threading.Thread(target=receive_camera, args=(4001,"Cam2")).start()
 
 while True:
-
-    # receive frame size
-    while len(data) < 4:
-        packet = conn.recv(4096)
-        if not packet:
-            break
-        data += packet
-
-    packed_size = data[:4]
-    data = data[4:]
-    frame_size = struct.unpack("I", packed_size)[0]
-
-    # receive frame data
-    while len(data) < frame_size:
-        data += conn.recv(4096)
-
-    frame_data = data[:frame_size]
-    data = data[frame_size:]
-
-    # decode image
-    frame = cv2.imdecode(
-        np.frombuffer(frame_data, dtype=np.uint8),
-        cv2.IMREAD_COLOR
-    )
-
-    if frame is None:
-        continue
-
-    cv2.imshow("Unity Camera", frame)
-
-    if cv2.waitKey(1) & 0xFF == 27:
+    if cv2.waitKey(1) == 27:
         break
-
-conn.close()
-cv2.destroyAllWindows()
